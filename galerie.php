0791ae95e4e513835ee779bc3c9de47c451f0983
LordDragons <149957558+LordDragons@users.noreply.github.com>
2024-07-16 11:07:59 +0200

    Update galerie.php

---
+++ galerie.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Galerie</title>
+    <link href="fansite.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php
+    include_once 'header.php'
+    ?>
+    <section class="galerie">
+        <div class="tour">
+            <a href="dragonsDansLart.php#D&D"><img src="images/tiamat.jpg" alt="Tiamat Reine mére des dragons"></a>
+        </div>
+        <div class="tour">
+            <a href="dragonsDansLart.php#ff7"><img src="images/bahamut.jpg" alt="Bahamut dans FF7"><a>
+        </div>
+        <div class="tour">
+            <a href="dragonsDansLart.php#ff7"><img src="images/bahamut2.jpg" alt="Néo Bahamut dans FF7"><a>
+        </div>
+        <div class="tour">
+            <a href="dragonsDansLart.php#ff7"><img src="images/bahamut3.jpg" alt="Bahamut Zéro dans FF7"><a>
+        </div>
+        <div class="tour">
+            <a href="dragonsDansHistoire.php#apocalypse"><img src="images/bete-apocalypse.jpg" alt="Apocalypse de Jean : 12. Le monstre marin et la bête avec la corne d'agneau. Gravure sur bois d'Albrecht Dürer, en 1497 ou 1498."><a>
+        </div>
+        <div class="tour">
+            <a href="sortOfDragons.php#africain"><img src="images/africain.jpg" alt="Mokele-Mbembe"><a>
+        </div>
+        <div class="tour">
+            <a href="sortOfDragons.php#chinois"><img src="images/chinois.jpg" alt="Un Dragon Chinois"><a>
+        </div>
+        <div class="tour">
+            <a href="dragonsDansLart.php#cinema"><img src="images/Coeur-de-dragon.jpg" alt="Dragon du film Coeur de Dragon"><a>
+        </div>
+        <div class="tour">
+            <a href="dragonsDansLart.php#cinema"><img src="images/dragon-3.jpg" alt="Krokmou et la Furie éclair Blanche du film Dragons3"><a>
+        </div>
+        <div class="tour">
+            <a href="colorOfDragons.php#or"><img src="images/dragon-d'or.jpg" alt="Un Dragon d'or"><a>
+        </div>
+        <div class="tour">
+            <a href="colorOfDragons.php#noir"><img src="images/dragon-noir.jpeg" alt="Un Dragon noir crachant du feu"><a>
+        </div>
+        <div class="tour">
+            <a href="sortOfDragons.php#occidental"><img src="images/dragon-occidental.jpeg" alt="Un imposant Draggon occidental"><a>
+        </div>
+        <div class="tour">
+            <a href="colorOfDragons.php#rouge"><img src="images/dragon-rouge.jpeg" alt="Un Dragon rouge volant dans les airs"><a>
+        </div>
+        <div class="tour">
+            <a href="colorOfDragons.php#vert"><img src="images/dragon-vert.jpeg" alt="Un Dragon Vert sortant d'une fôret en criant"><a>
+        </div>
+        <div class="tour">
+            <a href="dragonsDansLart.php#yugioh"><img src="images/DragonBlancauxYeuxBleus.jpg" alt="Carte d'un Dragon blanc aux yeux bleus"><a>
+        </div>
+        <div class="tour">
+            <a href="sortOfDragons.php#drug"><img src="images/druk.jpg" alt="Un Druk ou Dragon tonnerre"><a>
+        </div>
+        <div class="tour">
+            <a href="dragonsDansLart.php#anime"><img src="images/fairy-tail.jpg" alt="Natsu Dragnir le héros de Fairy Tail"><a>
+        </div>
+        <div class="tour">
+            <a href="colorOfDragons.php#bleu"><img src="images/dragon-bleu.jpg" alt="Un majestueux dragon bleu"><a>
+        </div>
+        <div class="tour">
+            <a href="sortOfDragons.php#gaas"><img src="images/gaasyendietha..jpeg" alt=""><a>
+        </div>
+        <div class="tour">
+            <a href="dragonsDansLart.php#hsf"><img src="images/histoire-sans-fin.jpg" alt="Fuchur  avec Bastien sur son dos"><a>
+        </div>
+        <div class="tour">
+            <a href="sortOfDragons.php#hydre"><img src="images/hydre.jpg" alt="Une Hydre à 8 têtes"><a>
+        </div>
+        <div class="tour">
+            <a href="colorOfDragons.php#blanc"><img src="images/dragon-blanc.jpg" alt="Un dragon blanc contre un groupe d'aventurier"><a>
+        </div>
+        <div class="tour">
+            <a href="sortOfDragons.php#japonais"><img src="images/japonais.jpg" alt=""><a>
+        </div>
+        <div class="tour">
+            <a href="dragonsDansLart.php#hunter"><img src="images/monster-hunter.jpg" alt=""><a>
+        </div>
+        <div class="tour">
+            <a href="sortOfDragons.php#quetza"><img src="images/quetzalcoatl.jpeg" alt=""><a>
+        </div>
+        <div class="tour">
+            <a href="dragonsDansLart.php#cinema"><img src="images/regne-du-feu.jpg" alt="Dragon volant, crachant du feu"><a>
+        </div>
+        <div class="tour">
+            <a href="dragonsDansHistoire.php#georges"><img src="images/saint-georges.jpg" alt=""><a>
+        </div>
+        <div class="tour">
+            <a href="dragonsDansLart.php#anime"><img src="images/shenron.jpg" alt=""><a>
+        </div>
+        <div class="tour">
+            <a href="dragonsDansLart.php#smaug"><img src="images/smaug.jpg" alt=""><a>
+        </div>
+        <div class="tour">
+            <a href="dragonsDansHistoire.php#tarasque"><img src="images/tarasque.jpg" alt=""><a>
+        </div>
+        <div class="tour">
+            <a href="sortOfDragons.php#vishap"><img src="images/vishap.jpeg" alt=""><a>
+        </div>
+        <div class="tour">
+            <a href="sortOfDragons.php#wyvern"><img src="images/Wyvern.jpg" alt=""><a>
+        </div>
+        <div class="tour">
+            <a href="dragonsDansHistoire.php#herensuge"><img src="images/Herensuge.png" alt=""><a>
+        </div>
+
+    </section>
+
+    <?php
+    include_once 'footer.php'
+    ?>
+    <script src="fansite.js"></script>
+</body>
+
+</html>
