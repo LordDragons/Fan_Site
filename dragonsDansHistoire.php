cc96f66af0114ec0a57cd0d99f12b9cd403ca2e6
LordDragons <149957558+LordDragons@users.noreply.github.com>
2024-07-16 11:10:12 +0200

    Mise à jour
    Ok pour moi

--- dragonsDansHistoire.php
+++ dragonsDansHistoire.php
@@ -9,31 +9,16 @@
 </head>
 
 <body>
-    <header>
-        <h1><a href="index.html">Les Dragons</a></h1>
-        <nav class="navbar">
-            <ul class="nav-list">
-                <li><a href="sortOfDragons.html">10 types de Dragons</a></li>
-                <li><a href="colorOfDragons.html">Les Couleurs des Dragons</a></li>
-                <li><a href="dragonsDansLart.html">Les Dragons Dans L'art</a></li>
-                <li><a href="dragonsDansHistoire.html">Evénéments</a></li>
-                <li><a href="célébrations.html">Actualités</a></li>
-                <li><a href="galerie.html">Galerie</a></li>
-            </ul>
-            <div class="burger-menu">
-                <div class="bar"></div>
-                <div class="bar"></div>
-                <div class="bar"></div>
-            </div>
-        </nav>
-    </header>
+    <?php
+    include_once 'header.php'
+    ?>
     <h2>Les sauroctones</h2>
     <p>
         Du grec σαῦρος / saûros, « lézard » et κτόνος / któnos, « tueur » ; littéralement « tueur de lézard » sont des
         personnes, ayant chassé, tué, soumis ou dompté des dragons, vouivres et cocatrix.
     </p>
-    <article class=dragon-histoire>
-        <h3>Bête de l'Apocalypse</h3>
+    <article class="dragon-histoire">
+        <h3 id="apocalypse">Bête de l'Apocalypse</h3>
         <p>
             La Bête de l'Apocalypse, (τὸ Θηρίον (tò thēríon) en grec ancien) est une figure de l'eschatologie chrétienne
             qui apparait dans le chapitre 13 de l'Apocalypse de Jean, en écho à la Vision des quatre bêtes du Livre de
@@ -44,7 +29,7 @@
         </p>
         <a href="https://fr.wikipedia.org/wiki/B%C3%AAte_de_l%27Apocalypse"><img src="images/bete-apocalypse.jpg" alt="Apocalypse de Jean : 12. Le monstre marin et la bête avec la corne d'agneau. Gravure sur bois d'Albrecht Dürer, en 1497 ou 1498."></a>
 
-        <h3>Saint Georges</h3>
+        <h3 id="georges">Saint Georges</h3>
         <p>
             Georges de Lydda (vers 275/280 - 23 avril 303), saint Georges, est un martyr du ive siècle, selon la
             tradition continue de l'Église catholique et des diverses Églises orthodoxes. Il est le saint patron, entre
@@ -58,7 +43,7 @@
 
     </article>
 
-    <article class=dragon-histoire>
+    <article class="dragon-histoire">
         <h3>Siegfried</h3>
         <p>
             C'est un héros de la Chanson des Nibelungen, la version continentale et christianisée du mythe, également
@@ -72,24 +57,27 @@
             Il débarrassa la ville de Metz du Graouilly qui le suiva jusqu'à la Seille, dans laquelle il se jetta avec
             sept petits Graouilly, juste après que le saint homme se soit signé.
         </p>
-        <a href="https://fr.wikipedia.org/wiki/Cl%C3%A9ment_de_Metz"><img src="images/St-Clément.jpg"
-        alt="Saint Clément conduit le Graouilly sur les bords de la Seille."></a>
+        <a href="https://fr.wikipedia.org/wiki/Cl%C3%A9ment_de_Metz"><img src="images/St-Clément.jpg" alt="Saint Clément conduit le Graouilly sur les bords de la Seille."></a>
     </article>
-    <footer class="footer">
-        <ul class="reseau">
-            <li class="reseau"><a href="" title="Suivez sur Facebook"
-                    target="_blank"><img class="icon" src="imgreseau/facebook.jpg"><span aria-hidden="true"></span></a></li>
-            <li class="reseau"><a href="" title="Suivez sur Instagram"
-                    target="_blank"><img class="icon" src="imgreseau/OIP.jpg"><span aria-hidden="true"></span></a></li>
-            <li class="reseau"><a href="" title="Suivez sur LinkedIn"
-                    target="_blank"><img class="icon" src="imgreseau/linkedin.jpg"><span aria-hidden="true"></span></a></li>
-            <li class="reseau"><a href="" title="Suivez sur Twitter"
-                    target="_blank"><img class="icon" src="imgreseau/twitter.jpg"><span aria-hidden="true"></span></a></li>
-            <li class="reseau"><a href="" title="Suivez sur Youtube"
-                    target="_blank"><img class="icon" src="imgreseau/youtube.jpg"><span aria-hidden="true"></span></a></li>
-        </ul>
-
-    </footer>
+    <article class="dragon-histoire">
+        <h3 id="tarasque">La Tarasque de Tarascon</h3>
+        <p>
+            Dragon amphibie aux yeux rougis et à l'haleine putride, la Tarasque vivait sur le rocher où a été construit le château de Tarascon. Elle guettait les voyageurs passant le Rhône pour s'en repaître, semant la terreur dans tout Tarascon.
+            Elle est décrite de la façon suivante par Jacques de Voragine dans la Légende dorée qu'il écrivit dans les années 1261-1266 : « Il y avait, à cette époque, sur les rives du Rhône, dans un marais entre Arles et Avignon, un dragon, moitié animal, moitié poisson, plus épais qu'un bœuf, plus long qu'un cheval, avec des dents semblables à des épées et grosses comme des cornes ; il se cachait dans le fleuve d'où il ôtait la vie à tous les passants et submergeait les navires3.
+        </p>
+        <a href="https://fr.wikipedia.org/wiki/Tarasque#:~:text=La%20Tarasque%20(%5Bta.,passage%20et%20terrorisant%20la%20population"><img src="images/tarasque.jpg" alt="La Tarasque"></a>
+    
+        <h3 id="herensuge">Herensuge</h3>
+    <p>
+    Herensuge est un serpent gigantesque dans la mythologie basque, variante de dragon. Il vole dans les airs et est généralement représenté avec sept grosses têtes.
+Herensuge est l'une des créatures les plus importantes de la mythologie basque. Ses noms varient mais la permanence du suffixe « suge » (couleuvre, serpent) suffit à justifier sa nature. C'est en effet un serpent.
+On trouve les noms de Erensugue (la prononciation -sugue est retranscrite avec ou sans « u »), Erensuge, Iraunsuge, Lerensuge, Herainsuge, Errensuge, Hensuge, Edensugue, Edeinsuge, Edaansuge, Igensuge, Iraunsuge.
+    </p>
+    <a href="https://fr.wikipedia.org/wiki/Herensuge"><img src="images/Herensuge.png" alt="Herensuge volant dans le ciel Basque"></a>
+    </article>
+    <?php
+    include_once 'footer.php'
+    ?>
     <script src="fansite.js"></script>
 </body>
 
