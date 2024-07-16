a85cb13b2a38261b1775cc435884b7663608b17b
LordDragons <149957558+LordDragons@users.noreply.github.com>
2024-07-16 11:02:16 +0200

    Mise à jour
    Mise à jour de la galerie

---
+++ célébrations.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actualités</title>
+    <link href="fansite.css" rel="stylesheet">
+</head>
+
+<body>
+<?php
+    include_once 'header.php'
+    ?>
+<article class="container">
+<img src="images/enCours.png">
+</article>
+<?php
+    include_once 'footer.php'
+    ?>
+    <script src="fansite.js"></script>
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,15 @@
+<footer class="footer">
+        <ul class="reseau">
+            <li class="reseau"><a href="" title="Suivez sur Facebook"
+                    target="_blank"><img class="icon" src="./imgreseau/facebook.jpg"><span aria-hidden="true"></span></a></li>
+            <li class="reseau"><a href="" title="Suivez sur Instagram"
+                    target="_blank"><img class="icon" src="imgreseau/OIP.jpg"><span aria-hidden="true"></span></a></li>
+            <li class="reseau"><a href="" title="Suivez sur LinkedIn"
+                    target="_blank"><img class="icon" src="imgreseau/linkedin.jpg"><span aria-hidden="true"></span></a></li>
+            <li class="reseau"><a href="" title="Suivez sur Twitter"
+                    target="_blank"><img class="icon" src="imgreseau/twitter.jpg"><span aria-hidden="true"></span></a></li>
+            <li class="reseau"><a href="" title="Suivez sur Youtube"
+                    target="_blank"><img class="icon" src="imgreseau/youtube.jpg"><span aria-hidden="true"></span></a></li>
+        </ul>
+
+</footer>
